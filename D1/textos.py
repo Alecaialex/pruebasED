4b33c92effe7aa3fba2f02708220018088a7fe79
alex <alecaialex@gmail.com>
2025-02-19 20:33:10 +0100

    clase

---
+++ D1/textos.py
@@ -0,0 +1,10 @@
+print("Textos 1")
+print("")
+print("Textos 2")
+
+# Comentario de 1 línea
+
+'''
+Comentario de
+varias líneas
+'''
---
+++ General/Variables y operaciones.py
@@ -0,0 +1,26 @@
+nombre = "Alex"
+print(f"Nombre: {nombre}")
+print(len(nombre))
+
+edad = 18
+print(f"Edad: {edad}")
+
+isConductor = True
+isXiJinPing = False
+
+print(f"Es conductor? {isConductor}")
+print(f"Es Xi Jinping? {isXiJinPing}")
+
+datoNulo = None
+print(f"Dato nulo: {datoNulo}")
+
+numero1 = 3
+numero2 = 4
+print(f"Suma: {numero1 + numero2}")
+print(f"Resta: {numero1 - numero2}")
+print(f"Multiplicación: {numero1 * numero2}")
+print(f"División: {numero1 / numero2}")
+print(f"División entera: {numero1 // numero2}")
+print(f"Módulo: {numero1 % numero2}")
+print(f"Potencia: {numero1 ** numero2}")
+print(f"Raiz cuadrada: {numero1 ** 0.5}")
