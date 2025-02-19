09a7455afcb9b60a668aa5a22da3dd0080834fa3
alex <alecaialex@gmail.com>
2025-02-19 20:45:25 +0100

    clase

--- General/Variables y operaciones.py
+++ General/Variables y operaciones.py
@@ -24,3 +24,10 @@ print(f"División entera: {numero1 // numero2}")
 print(f"Módulo: {numero1 % numero2}")
 print(f"Potencia: {numero1 ** numero2}")
 print(f"Raiz cuadrada: {numero1 ** 0.5}")
+
+nombres = ["Alex", "Pepe", "Juan", "Xin Huang"]
+print(f"Nombres: {nombres}")
+print(f"Primer nombre: {nombres[0]}")
+print(f"Último nombre: {nombres[-1]}")
+print(f"Sublista: {nombres[1:3]}")
+print(f"Longitud: {len(nombres)}")
