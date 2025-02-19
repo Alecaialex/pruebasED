d3b0bf8e65f86106fe8c8e7ebe68887d74a610bf
alex <alecaialex@gmail.com>
2025-02-19 19:29:50 +0100

    main

---
+++ main.py
@@ -0,0 +1,21 @@
+patatinpatatan = "Shiver me timbers"
+shivermetimbers = True
+password = input("Introduce tu contraseña:")
+
+print(patatinpatatan + "cause im sailing away")
+
+if shivermetimbers:
+    print("cause im sailing away")
+else:
+    print("dont shiver me ):")
+
+if len(password) < 10 :
+    print("Tremenda basura")
+else:
+    print("Ta wena")
+
+
+真夜中 = 3
+while 真夜中 < 2:
+    print("hola")
+    真夜中 = 真夜中+1
